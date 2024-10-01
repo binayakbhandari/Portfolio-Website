fa5ed644a9313d01f105bec2cf22aa523f0a1098
Bikram Gyawali <157723263+CoddingJourney@users.noreply.github.com>
2024-10-01 07:54:22 -0800

    navigate to persons not person

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -45,7 +45,7 @@ function Form({ type, id }) {
         } else {
             const response = await axios.put("https://66dc946947d749b72acbfa21.mockapi.io/persons/" + id, data)
             if (response.status === 200) {
-                navigate('/person/' + id)
+                navigate('/persons/' + id)
             } else {
                 alert("Failed to edit the profile.")
             }
@@ -53,7 +53,7 @@ function Form({ type, id }) {
     }
 
     const cancelEdit = ()=>{
-        navigate(`/person/${id}`)
+        navigate(`/persons/${id}`)
     }
 
     return (
