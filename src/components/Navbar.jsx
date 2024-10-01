f83d95f73b86d1f4f1d0a575ebe313beb882f6c6
binayakbhandari <binayakbhandari60@gmail.com>
2024-10-02 02:01:20 +0545

    error solved

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,128 +1,75 @@
-import { useState } from 'react'
-import { Link } from 'react-router-dom'
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 
-function Navbar() {
-    const defaultImage = "https://static-00.iconduck.com/assets.00/user-icon-2048x2048-ihoxz4vq.png"
-    const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false)
+const Navbar = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
 
-    const toggleMobileMenu = () => {
-        setIsMobileMenuOpen(!isMobileMenuOpen)
-    };
+  return (
+    <nav className="sticky border-gray-200 bg-gray-50 dark:bg-gray-800 dark:border-gray-700">
+      <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+        <Link to="/" className="flex items-center space-x-3 rtl:space-x-reverse">
+          <img
+            src="https://flowbite.com/docs/images/logo.svg"
+            className="h-8"
+            alt="Flowbite Logo"
+          />
+          <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
+            MERN3.0
+          </span>
+        </Link>
+        <button
+          onClick={() => setIsMenuOpen(!isMenuOpen)}
+          type="button"
+          className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+          aria-controls="navbar-solid-bg"
+          aria-expanded={isMenuOpen}
+        >
+          <span className="sr-only">Open main menu</span>
+          <svg
+            className="w-5 h-5"
+            aria-hidden="true"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 17 14"
+          >
+            <path
+              stroke="currentColor"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="M1 1h15M1 7h15M1 13h15"
+            />
+          </svg>
+        </button>
+        <div
+          className={`${
+            isMenuOpen ? 'block' : 'hidden'
+          } w-full md:block md:w-auto`}
+          id="navbar-solid-bg"
+        >
+          <ul className="flex flex-col font-medium mt-4 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-transparent dark:bg-gray-800 md:dark:bg-transparent dark:border-gray-700">
+            <li>
+              <Link
+                to="/create"
+                className="rounded-md bg-gray-500 px-3 py-2 text-sm font-medium text-white"
+                aria-current="page"
+              >
+                + Create Blog
+              </Link>
+            </li>
+            <li>
+              <Link
+                to="/contact"
+                className="block py-2 px-3 md:p-0 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+              >
+                Contact
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+};
 
-    return (
-        <>
-            <nav className="bg-gray-800">
-                <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
-                    <div className="relative flex h-16 items-center justify-between">
-                        <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
-                            {/* Mobile menu button */}
-                            <button
-                                type="button"
-                                className="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
-                                aria-controls="mobile-menu"
-                                aria-expanded={isMobileMenuOpen ? 'true' : 'false'}
-                                onClick={toggleMobileMenu}
-                            >
-                                <span className="absolute -inset-0.5"></span>
-                                <span className="sr-only">Open main menu</span>
-                                {/* Icon when menu is closed */}
-                                <svg
-                                    className={`${isMobileMenuOpen ? 'hidden' : 'block'} h-6 w-6`}
-                                    fill="none"
-                                    viewBox="0 0 24 24"
-                                    strokeWidth="1.5"
-                                    stroke="currentColor"
-                                    aria-hidden="true"
-                                >
-                                    <path
-                                        strokeLinecap="round"
-                                        strokeLinejoin="round"
-                                        d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
-                                    />
-                                </svg>
-                                {/* Icon when menu is open */}
-                                <svg
-                                    className={`${isMobileMenuOpen ? 'block' : 'hidden'} h-6 w-6`}
-                                    fill="none"
-                                    viewBox="0 0 24 24"
-                                    strokeWidth="1.5"
-                                    stroke="currentColor"
-                                    aria-hidden="true"
-                                >
-                                    <path
-                                        strokeLinecap="round"
-                                        strokeLinejoin="round"
-                                        d="M6 18L18 6M6 6l12 12"
-                                    />
-                                </svg>
-                            </button>
-                        </div>
-
-                        <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
-                            <div className="flex flex-shrink-0 items-center animate-rotating">
-                                <Link to="/">
-                                    <img
-                                        className="h-8 w-auto"
-                                        src="https://cdn0.iconfinder.com/data/icons/logos-brands-in-colors/128/react_color-512.png"
-                                        alt="Logo"
-                                    />
-                                </Link>
-                            </div>
-                            <div className="hidden sm:ml-6 sm:block">
-                                <div className="flex space-x-4">
-                                    <Link
-                                        to="/create"
-                                        className="rounded-md bg-gray-900 px-3 py-2 text-sm font-medium text-white"
-                                        aria-current="page"
-                                    >
-                                        + Create Card
-                                    </Link>
-                                    <Link
-                                        to="/contact"
-                                        className="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white"
-                                    >
-                                        Contact
-                                    </Link>
-                                </div>
-                            </div>
-                        </div>
-                        <div>
-                            <Link to="https://www.linkedin.com/in/binayak-bhandari/">
-                                <button type="button" className="relative flex rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800" id="user-menu-button" aria-expanded="false" aria-haspopup="true">
-                                    <span className="absolute -inset-1.5"></span>
-                                    <span className="sr-only">Open user menu</span>
-                                    <img className="h-8 w-8 rounded-full" src="https://avatars.githubusercontent.com/u/173297872?s=400&v=4" onError={(e) => e.target.src = defaultImage} alt="" />
-                                </button>
-                            </Link>
-                        </div>
-                    </div>
-                </div>
-
-                {/* Mobile menu */}
-                <div
-                    className={`sm:hidden ${isMobileMenuOpen ? 'block' : 'hidden'}`}
-                    id="mobile-menu"
-                >
-                    <div className="space-y-1 px-2 pb-3 pt-2">
-                        <Link
-                            to="/create"
-                            className="block rounded-md bg-gray-900 px-3 py-2 text-base font-medium text-white"
-                            aria-current="page"
-                        >
-                            + Create Card
-                        </Link>
-                        <Link
-                            to="/contact"
-                            className="block rounded-md px-3 py-2 text-base font-medium text-gray-300 hover:bg-gray-700 hover:text-white"
-                        >
-                            Contact
-                        </Link>
-                    </div>
-                </div>
-            </nav>
-        </>
-
-    );
-}
-
-export default Navbar
+export default Navbar;
