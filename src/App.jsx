e62b631f418bf4f20de5fc333cd412fd89e02655
binayakbhandari <binayakbhandari60@gmail.com>
2025-04-07 23:32:17 +0545

    Changes made in portfolio website

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,33 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom"
-import Home from "./pages/home/Home"
-import CreateProduct from "./pages/create/CreateProduct"
-import EditProduct from "./pages/edit/EditProduct"
-import SingleProduct from "./pages/single/SingleProduct"
-import ContactMe from "./pages/contact/Contact"
 
+import Sidebar from './components/Sidebar';
 
 function App() {
-
   return (
-    <>
-      <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/create' element={<CreateProduct />} />
-        <Route path='/edit/:id' element={<EditProduct />} />
-        <Route path='/person/:id' element={<SingleProduct />} />
-        <Route path='/contact' element={<ContactMe />} />
-      </Routes>
-      </BrowserRouter>
-    </>
-  )
+    <div className="flex">
+      <Sidebar />
+      <main className="flex-1 p-6">Main Content Goes Here</main>
+    </div>
+  );
 }
 
-export default App
+export default App;
+
+
+// function App() {
+
+//   return (
+//     <>
+//       <BrowserRouter>
+//       <Routes>
+//         <Route path='/' element={<Home />} />
+//         <Route path='/create' element={<CreateProduct />} />
+//         <Route path='/edit/:id' element={<EditProduct />} />
+//         <Route path='/person/:id' element={<SingleProduct />} />
+//         <Route path='/contact' element={<ContactMe />} />
+//       </Routes>
+//       </BrowserRouter>
+//     </>
+//   )
+// }
+
+// export default App
