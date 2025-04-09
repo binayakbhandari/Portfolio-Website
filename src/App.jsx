4afd7586aa7e68dafc06782803f03a3a524ca576
binayakbhandari <binayakbhandari60@gmail.com>
2025-04-09 09:50:51 +0545

    Upddated portfolio

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { Analytics } from '@vercel/analytics/react';
 
 import { BrowserRouter, Route, Routes } from "react-router-dom"
 import About from "./pages/About"
@@ -20,6 +21,8 @@ function App() {
         <Route path='/contact' element={<Contact />} />
       </Routes>
       </BrowserRouter>
+      <Analytics />
+
     </>
   )
 }
