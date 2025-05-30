aad3396f615082a5f98032b84044fec9ffa95ffe
binayakbhandari <binayakbhandari60@gmail.com>
2025-05-30 23:28:57 +0545

    Changes Made  In Portfolio

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,33 @@
-import { Analytics } from '@vercel/analytics/react';
-
-import { BrowserRouter, Route, Routes } from "react-router-dom"
-import About from "./pages/About"
-import Blog from "./pages/Blog"
-import Contact from "./pages/Contact"
-import Home from "./pages/Home"
-import Projects from "./pages/Projects"
-
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
 
 function App() {
+  const [count, setCount] = useState(0)
 
   return (
     <>
-      <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/about' element={<About />} />
-        <Route path='/blog' element={<Blog />} />
-        <Route path='/projects' element={<Projects />} />
-        <Route path='/contact' element={<Contact />} />
-      </Routes>
-      </BrowserRouter>
-      <Analytics />
-
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
     </>
   )
 }
